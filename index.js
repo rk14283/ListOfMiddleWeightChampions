8b81e112bc6c13a2fe311d72d01cf9a6d22b829f
rk14283 <rk14283@my.bristol.ac.uk>
2022-11-17 08:09:36 +0100

    Boxer's record age column missing fixed

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const axios = require("axios");
 const { JSDOM } = require("jsdom");
 const fs = require("fs");
-
-//here the function finds record table and stops
-//function findRecordTable()
+const { fips } = require("crypto");
 
 function findRecordTable(tables) {
   //this function returns table with th
@@ -19,10 +17,8 @@ function findRecordTable(tables) {
   return null;
 }
 
-async function scrapeRecordTable() {
-  const response = await axios.get(
-    "https://en.wikipedia.org/wiki/Marvelous_Marvin_Hagler"
-  );
+async function scrapeRecordTable(url) {
+  const response = await axios.get(url);
   const html = response.data;
   const jsdom = new JSDOM(html);
   const document = jsdom.window.document;
@@ -35,58 +31,104 @@ async function scrapeRecordTable() {
   const [headings, ...rows] = tableToScrape.querySelectorAll("tr");
 
   let record = [];
+  let heads = [];
 
-  for (row of rows) {
-    const [
-      Number,
-      Result,
-      Record,
-      Opponent,
-      Type,
-      RoundTime,
-      Date,
-      // Age,not present for hagler
-      Location,
-      Notes,
-    ] = row.querySelectorAll("td");
+  if (headings.textContent[57] == "A") {
+    for (row of rows) {
+      const [
+        Number,
+        Result,
+        Record,
+        Opponent,
+        Type,
+        RoundTime,
+        Date,
+        Age,
+        Location,
+        Notes,
+      ] = row.querySelectorAll("td");
 
-    const opponentName = Opponent?.textContent;
-    const serialNumber = Number?.textContent;
-    const fightResult = Result?.textContent;
-    const tally = Record?.textContent;
-    const foughtAgainst = Opponent?.textContent;
-    const winType = Type?.textContent;
-    const endRound = RoundTime?.textContent;
-    const fightDate = Date?.textContent;
-    // const fighterAge = Age?.textContent;
-    const fightLocation = Location?.textContent;
-    const remarks = Notes?.textContent;
-    //console.log(opponentName,serialNumber,fightResult,tally,foughtAgainst,winType,endRound,fightDate,fighterAge,fightLocation,remarks);
+      const opponentName = Opponent?.textContent;
+      const serialNumber = Number?.textContent;
+      const fightResult = Result?.textContent;
+      const tally = Record?.textContent;
+      const foughtAgainst = Opponent?.textContent;
+      const winType = Type?.textContent;
+      const endRound = RoundTime?.textContent;
+      const fightDate = Date?.textContent;
+      const fighterAge = Age?.textContent;
+      const fightLocation = Location?.textContent;
+      const remarks = Notes?.textContent;
+      //console.log(opponentName,serialNumber,fightResult,tally,foughtAgainst,winType,endRound,fightDate,fighterAge,fightLocation,remarks);
 
-    const boxerRecord = {
-      No: serialNumber,
-      Result: fightResult,
-      Record: tally,
-      Opponent: foughtAgainst,
-      Type: winType,
-      Round_Time: endRound,
-      Date: fightDate,
-      // Age: fighterAge,
-      Location: fightLocation,
-      Notes: remarks,
-    };
-    //console.log(boxerRecord);
-    record.push(boxerRecord);
+      const boxerRecord = {
+        No: serialNumber,
+        Result: fightResult,
+        Record: tally,
+        Opponent: foughtAgainst,
+        Type: winType,
+        Round_Time: endRound,
+        Date: fightDate,
+        Age: fighterAge,
+        Location: fightLocation,
+        Notes: remarks,
+      };
+      //console.log(boxerRecord);
+      record.push(boxerRecord);
+    }
+    //console.log(record);
+    const json = JSON.stringify(record);
+    const recordBoxer = url.split("https://en.wikipedia.org/wiki/");
+    fs.writeFileSync(`${recordBoxer}.json`, json.replace(/\\n/g, ""));
+  } else {
+    for (row of rows) {
+      const [
+        Number,
+        Result,
+        Record,
+        Opponent,
+        Type,
+        RoundTime,
+        Date,
+        //Age,
+        Location,
+        Notes,
+      ] = row.querySelectorAll("td");
+
+      const opponentName = Opponent?.textContent;
+      const serialNumber = Number?.textContent;
+      const fightResult = Result?.textContent;
+      const tally = Record?.textContent;
+      const foughtAgainst = Opponent?.textContent;
+      const winType = Type?.textContent;
+      const endRound = RoundTime?.textContent;
+      const fightDate = Date?.textContent;
+      // const fighterAge = Age?.textContent;
+      const fightLocation = Location?.textContent;
+      const remarks = Notes?.textContent;
+      //console.log(opponentName,serialNumber,fightResult,tally,foughtAgainst,winType,endRound,fightDate,fighterAge,fightLocation,remarks);
+
+      const boxerRecord = {
+        No: serialNumber,
+        Result: fightResult,
+        Record: tally,
+        Opponent: foughtAgainst,
+        Type: winType,
+        Round_Time: endRound,
+        Date: fightDate,
+        // Age: fighterAge,
+        Location: fightLocation,
+        Notes: remarks,
+      };
+      //console.log(boxerRecord);
+      record.push(boxerRecord);
+    }
+    //console.log(record);
   }
-  //console.log(record);
   const json = JSON.stringify(record);
-  //this would require me to have knowledge of regex
-  fs.writeFileSync(`Marvin Hagler.json`, json.replace(/\\n/g, ""));
+  const recordBoxer = url.split("https://en.wikipedia.org/wiki/");
+  fs.writeFileSync(`${recordBoxer}.json`, json.replace(/\\n/g, ""));
   //console.log(json.replace(/\\n/g, ""));
 }
 
-async function scrapeChampions() {
-  const response = await axios.get(
-    "https://en.wikipedia.org/wiki/List_of_world_middleweight_boxing_champions"
-  );
-}
+scrapeRecordTable("https://en.wikipedia.org/wiki/Marvelous_Marvin_Hagler");
