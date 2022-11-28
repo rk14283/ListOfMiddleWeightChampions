2c77833c990b9932323c6cf58192cc9407f07d10
rk14283 <rk14283@my.bristol.ac.uk>
2022-11-28 09:00:37 +0100

    matchesReacherror

--- prisma/seeds/boxers.js
+++ prisma/seeds/boxers.js
@@ -17,41 +17,58 @@ async function seed() {
     //const boxer = require("../../boxers/Thomas_Hearns.json");
     //console.log(boxer);
     const boxersRecord = fs.readFileSync(files[i]);
-    readableBoxerRecord = JSON.parse(boxersRecord);
-    var matchesHeight = readableBoxerRecord.Height;
-    var regExp = /\(([^)]+)\)/;
-    var matchesReach = regExp.exec(readableBoxerRecord.Reach);
+    const readableBoxerRecord = JSON.parse(boxersRecord);
+    const Height = readableBoxerRecord.Height;
+    const Reach = readableBoxerRecord.Reach;
 
+    const regExp = /\(([^)]+)\)/;
+    ///Todo applyRegex to height
+    const matchesReach = regExp.exec(readableBoxerRecord.Reach);
+    const matchesHeight = regExp.exec(readableBoxerRecord.Height);
+    //const reachInt = parseInt(matchesReach);
+    //console.log(typeof matchesReach);
+
+    console.log("this is height", matchesHeight[1]);
+    //5
+
+    function removeByIndex(str, index) {
+      return str.slice(0, index) + str.slice(index + 1);
+    }
+    const updatedReach = removeByIndex(matchesReach[1], 5);
+    //console.log("this is reach", updatedReach);
+    // console.log("this is reach", matchesReach[1].split(5));
     const dateOfBirth = readableBoxerRecord?.Born;
     const deathDate = readableBoxerRecord?.Died;
     //console.log(dateOfBirth);
     // console.log(deathDate);
-    matchesDateOfBirth = dateOfBirth?.match(/\(([^()]*)\)/);
-    matchesDateOfDeath = deathDate?.match(/\(([^()]*)\)/);
+    const matchesDateOfBirth = dateOfBirth?.match(/\(([^()]*)\)/);
+    const matchesDateOfDeath = deathDate?.match(/\(([^()]*)\)/);
     //console.log(matchesDateOfDeath);
-    dateTimeFormat = new Date(matchesDateOfBirth);
+    const dateTimeFormat = new Date(matchesDateOfBirth);
+    let dateTimeFormatDeath = null;
     if (matchesDateOfDeath) {
       dateTimeFormatDeath = new Date(matchesDateOfDeath);
     }
     //console.log(matchesHeight);
     //console.log(dateTimeFormat);
     //console.log(dateTimeFormatDeath);
+
+    const inserted = await prisma.boxer.create({
+      data: {
+        imageURL: readableBoxerRecord.imageUrl?.substring(2),
+        name: readableBoxerRecord?.name,
+        nickName: readableBoxerRecord["Nickname(s)"]
+          ?.trim()
+          ?.replaceAll("\n", ", "),
+        height: parseInt(matchesHeight[1]),
+        reach: parseInt(updatedReach),
+        born: dateTimeFormat,
+        died: dateTimeFormatDeath,
+        stance: readableBoxerRecord.Stance,
+      },
+    });
+    console.log(inserted);
   }
-  const inserted = await prisma.readableBoxerRecord?.create({
-    data: {
-      imageURL: readableBoxerRecord.imageUrl?.substring(2),
-      name: readableBoxerRecord?.name,
-      nickName: readableBoxerRecord["Nickname(s)"]
-        ?.trim()
-        ?.replaceAll("\n", ", "),
-      height: parseInt(matchesHeight[1]),
-      reach: parseInt(matchesReach[1]),
-      born: dateTimeFormat,
-      died: dateTimeFormatDeath,
-      stance: readableBoxerRecord.Stance,
-    },
-  });
-  console.log(inserted);
 }
 
 seed();
