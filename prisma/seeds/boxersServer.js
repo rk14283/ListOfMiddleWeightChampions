6527c640b0ddc483f8b8005c59220d08bb15f75e
rk14283 <rk14283@my.bristol.ac.uk>
2022-12-02 07:37:41 +0100

    BoxersByIDFromDatabase

---
+++ prisma/seeds/boxersServer.js
@@ -0,0 +1,22 @@
+const express = require("express");
+//const boxers = require("boxers");
+const app = express();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+app.use(express.json()); //"use" ing middleware
+const PORT = process.env.PORT || 6000;
+
+app.get("/", (req, res) => {
+  res.send("This is the night of champions");
+});
+
+app.get("/boxers", async (req, res) => {
+  const boxers = await prisma.boxer.findMany();
+  // etc
+  res.json(boxers);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
